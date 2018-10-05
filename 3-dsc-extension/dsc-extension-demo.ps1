777caa4b968624c1ee5e6665454776c6eb19dfec
Neil Peterson <neilpeterson@outlook.com>
2018-10-05 08:02:37 -0700

    Script updates

--- 3-dsc-extension/dsc-extension-demo.ps1
+++ 3-dsc-extension/dsc-extension-demo.ps1
@@ -2,19 +2,19 @@
 Login-AzureRmAccount -credential $cred
 
 $resourcegroup = "ignitedemo1"
-$storageacct = "ignitedemodsc"
-$containername = "ignitedemodsc"
-$file = "c:\windows-dsc-iis-app.ps1"
+$storageacct = "dscstore007"
+$containername = "dscstore007"
+$file = "./windows-dsc-iis-app.ps1"
 $vm = "ignitedemo1"
 $config = "App"
 $blob = "windows-dsc-iis-app.ps1.zip"
 $config = "App"
 $version = "2.14"
-$location = "West US"
+$location = "eastus"
 
 
 # publish to azure storage
-Publish-AzureRmVMDscConfiguration -ResourceGroupName $resourcegroup -ConfigurationPath $file -StorageAccountName $storageacct -ContainerName $containername
+Publish-AzVMDscConfiguration -ResourceGroupName dsc-store -ConfigurationPath $file -StorageAccountName $storageacct -ContainerName $containername
 
 # appy to vm
 Set-AzureRmVMDscExtension -ResourceGroupName $resourcegroup -VMName $vm -ArchiveBlobName $blob -ArchiveStorageAccountName $storageacct -ConfigurationName $config -ArchiveContainerName $containername -Version $version -Location $location
