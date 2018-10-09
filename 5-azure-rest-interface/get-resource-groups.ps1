4d79360c7080a0c950b9e8acecd28773e9e276d5
Neil Peterson <nepeters@microsoft.com>
2018-10-08 20:38:52 -0700

    REST notes

--- 5-azure-rest-interface/get-resource-groups.ps1
+++ 5-azure-rest-interface/get-resource-groups.ps1
@@ -15,15 +15,15 @@ $TenantId = (Get-AzKeyVaultSecret -VaultName $vault -Name AzureTenantID).SecretV
 $ClientId = (Get-AzKeyVaultSecret -VaultName $vault -Name AzureClientID).SecretValueText
 $ClientSecret = (Get-AzKeyVaultSecret -VaultName $vault -Name AzureClientSecret).SecretValueText
 $SubscriptionId = (Get-AzKeyVaultSecret -VaultName $vault -Name AzureSubscriptionID).SecretValueText
-$Resource = "https://management.core.windows.net/"
 
-# Request
+# Acquire an access token
+$Resource = "https://management.core.windows.net/"
 $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
 $Body = "grant_type=client_credentials&client_id=$ClientId&client_secret=$ClientSecret&resource=$Resource"
 $Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $Body -ContentType 'application/x-www-form-urlencoded'
 
-
-$ResourceGroupApiUri = "https://management.azure.com/subscriptions/$SubscriptionId/resourcegroups?api-version=2017-05-10"
+# Query RG Endpoint
+$ResourceGroupApiUri = "https://management.azure.com/subscriptions/$SubscriptionId/resourcegroups?api-version=2018-02-01"
 $Headers = @{}
 $Headers.Add("Authorization","$($Token.token_type) "+ " " + "$($Token.access_token)")
 $ResourceGroups = Invoke-RestMethod -Method Get -Uri $ResourceGroupApiUri -Headers $Headers
